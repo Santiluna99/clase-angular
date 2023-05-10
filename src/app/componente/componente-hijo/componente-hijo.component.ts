27e1aee1107bbcd17ee487b625d2ac87759914e0
santlunaschiavi <santlunaschiavi@gmail.com>
2023-05-10 18:26:48 -0300

    comentarios

--- src/app/componente/componente-hijo/componente-hijo.component.ts
+++ src/app/componente/componente-hijo/componente-hijo.component.ts
@@ -9,5 +9,5 @@ export class ComponenteHijoComponent {
   valor:string=""
   edad:number=0
   amigos:string=""
-//hago declaraciones de los componentes con su respetivo tipo
+//hago declaraciones de los componentes con su respectivo tipo
 }
