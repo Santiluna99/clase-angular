f99ba756f9a3a917fafb996b744d56bfa15a5dea
santlunaschiavi <santlunaschiavi@gmail.com>
2023-05-10 18:22:48 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteHijoComponent } from './componente/componente-hijo/componente-hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteHijoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componente/componente-hijo/componente-hijo.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-componente-hijo',
+  templateUrl: './componente-hijo.component.html',
+  styleUrls: ['./componente-hijo.component.css']
+})
+export class ComponenteHijoComponent {
+  valor:string=""
+  edad:number=0
+  amigos:string=""
+//hago declaraciones de los componentes con su respetivo tipo
+}
